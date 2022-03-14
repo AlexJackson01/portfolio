18a028384e33e574faaa04c4b1488c588f89fe36
AlexJackson01 <ajjackson22@gmail.com>
2022-03-14 17:41:05 +0000

    Tweaked information on Middle

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,14 +1,33 @@
 import React from 'react';
 import Fade from 'react-reveal/Fade';
 import Me from "../me.png";
+import Button from '@mui/material/Button';
+import '../../src/App.css';
+
+
 
 export default function About() {
   return (
     <div className='sub-section'>
         <Fade bottom>
-        <div className="information">
-            <h4>About Me</h4>
-            <p>Hello, my name is Alex Jackson. I am a Front-End Developer....</p>
+        <div>
+            <h4>Welcome</h4>
+            <p>Hello, I'm Alex and I'm based in London, UK. I am a Full-Stack Developer with over nine years of experience in client relations, project management and automation.</p>
+            <p>I thrive in any situation where I can work collaboratively / independently to solve complex problems, and meet deadlines or objectives. I’m a champion for #womenintech and I’m highly passionate about how advancements in the tech field can be used for good. I’m interested in finding an opportunity that allows me to create impactful products that make a real positive difference to businesses and their customers.</p>
+            <p>I specialise in writing clean, testable code as well as designing and building visually appealing full stack applications with users front of mind, using technologies such as Javascript, React, MySQL, Node/ExpressJS, GraphQL, Firebase/Cloud Firestore and more.</p>
+            <p>If you'd like to have a chat about my projects or job opportunities, please drop me an email using the contact form below. I welcome all feedback and messages to further my journey into tech.</p>
+            <div className='see-projects'>
+          <Button variant="contained" href="#projects" size="large" sx={{fontFamily: "Montserrat", backgroundColor: "#e3997c", "&:hover": {backgroundColor: "#e8ad96", color: "#486b53"}}}>
+          Check Out My Projects
+          </Button>
+          </div>
+
+
+
+            
+
+
+
     </div>
     <div className="headshot-container">
         <img className="headshot" src={Me} alt="Alex Jackson headshot" />
--- portfolio/src/components/projects/middle/Middle.js
+++ portfolio/src/components/projects/middle/Middle.js
@@ -36,14 +36,6 @@ const items = [
           name: "Search Page",
           Image: SearchPage
         },
-        {
-          name: "Figma Design",
-          Image: Figma_1
-        },
-        {
-          name: "Figma Design",
-          Image: Figma_2
-        },
     ]
 
 
@@ -85,30 +77,34 @@ export default function Middle() {
             </div>
         <div className='col-6'>
             <h2>Meet in the Middle</h2>
-            <h6>Full Stack</h6>
             <div className='tech-stack'>
-
-
-            <Stack className="tech-icons" direction="row" spacing={1} sx={{color: "white", display: "block", margin: "0"}}>
-            <Chip icon={<Javascript style={{color: "white"}}/>} label="Javascript" variant="Filled" sx={{color: "white", fontFamily: "Montserrat"}}/>
-            <Chip icon={<Hub style={{color: "white"}}/>} label="React" variant="Filled" sx={{color: "white", fontFamily: "Montserrat"}} />
-            <Chip icon={<Css style={{color: "white"}}/>} label="CSS/Bootstrap" variant="Filled" sx={{color: "white", fontFamily: "Montserrat"}} />
-            <Chip icon={<GpsFixed style={{color: "white"}}/>} label="TomTom API" variant="Filled" sx={{color: "white", fontFamily: "Montserrat"}} />
-            <Chip icon={<Brush style={{color: "white"}}/>} label="Figma & Canva" variant="Filled" sx={{color: "white", fontFamily: "Montserrat"}} />
-            <Chip icon={<Room style={{color: "white"}}/>} label="React Leaflet" variant="Filled" sx={{color: "white", fontFamily: "Montserrat"}} />
-            <Chip icon={<Room style={{color: "white"}}/>} label="Openroute Service API" variant="Filled" sx={{color: "white", fontFamily: "Montserrat"}} />
-            <Chip icon={<Room style={{color: "white"}}/>} label="Google Maps API" variant="Filled" sx={{color: "white", fontFamily: "Montserrat"}} />
-            <Chip icon={<Route style={{color: "white"}}/>} label="React Router" variant="Filled" sx={{color: "white", fontFamily: "Montserrat"}} />
-            <Chip icon={<ThumbUp style={{color: "white"}}/>} label="Font Awesome" variant="Filled" sx={{color: "white", fontFamily: "Montserrat"}} />
-            <Chip icon={<Hub style={{color: "white"}}/>} label="Axios" variant="Filled" sx={{color: "white", fontFamily: "Montserrat"}} />
-            <Chip icon={<LocalFireDepartment style={{color: "white"}}/>} label="Firebase" variant="Filled" sx={{color: "white", fontFamily: "Montserrat"}}/>
-            <Chip icon={<LocalFireDepartment style={{color: "white"}}/>} label="Cloud Firestore" variant="Filled" sx={{color: "white", fontFamily: "Montserrat"}} />
-            <Chip icon={<Email style={{color: "white"}}/>} label="EmailJS" variant="Filled" sx={{color: "white", fontFamily: "Montserrat"}} />
-            </Stack>
+              <Stack className="tech-icons" direction="row" spacing={1} sx={{color: "white", display: "block", margin: "0"}}>
+              <Chip icon={<Javascript style={{color: "white"}}/>} label="Javascript" variant="Filled" sx={{color: "white", fontFamily: "Montserrat"}}/>
+              <Chip icon={<Hub style={{color: "white"}}/>} label="React" variant="Filled" sx={{color: "white", fontFamily: "Montserrat"}} />
+              <Chip icon={<Css style={{color: "white"}}/>} label="CSS/Bootstrap" variant="Filled" sx={{color: "white", fontFamily: "Montserrat"}} />
+              <Chip icon={<GpsFixed style={{color: "white"}}/>} label="TomTom API" variant="Filled" sx={{color: "white", fontFamily: "Montserrat"}} />
+              <Chip icon={<Brush style={{color: "white"}}/>} label="Figma & Canva" variant="Filled" sx={{color: "white", fontFamily: "Montserrat"}} />
+              <Chip icon={<Room style={{color: "white"}}/>} label="React Leaflet" variant="Filled" sx={{color: "white", fontFamily: "Montserrat"}} />
+              <Chip icon={<Room style={{color: "white"}}/>} label="Openroute Service API" variant="Filled" sx={{color: "white", fontFamily: "Montserrat"}} />
+              <Chip icon={<Room style={{color: "white"}}/>} label="Google Maps API" variant="Filled" sx={{color: "white", fontFamily: "Montserrat"}} />
+              <Chip icon={<Route style={{color: "white"}}/>} label="React Router" variant="Filled" sx={{color: "white", fontFamily: "Montserrat"}} />
+              <Chip icon={<ThumbUp style={{color: "white"}}/>} label="Font Awesome" variant="Filled" sx={{color: "white", fontFamily: "Montserrat"}} />
+              <Chip icon={<Hub style={{color: "white"}}/>} label="Axios" variant="Filled" sx={{color: "white", fontFamily: "Montserrat"}} />
+              <Chip icon={<LocalFireDepartment style={{color: "white"}}/>} label="Firebase" variant="Filled" sx={{color: "white", fontFamily: "Montserrat"}}/>
+              <Chip icon={<LocalFireDepartment style={{color: "white"}}/>} label="Cloud Firestore" variant="Filled" sx={{color: "white", fontFamily: "Montserrat"}} />
+              <Chip icon={<Email style={{color: "white"}}/>} label="EmailJS" variant="Filled" sx={{color: "white", fontFamily: "Montserrat"}} />
+              </Stack>
             </div>
-            <p className='project-description'>If you’re looking to go <b>somewhere new</b>, if you’re meeting someone who <b>lives on the other side of town</b>, if you’re a couple going on a <b>date night</b>, someone organising a <b>first date</b>, colleagues going for a <b>work lunch</b>, friends or family having a <b>catch up</b>, or if you’re just plain <b>indecisive</b> about where to go, why not... <b><em>Meet in the Middle?</em></b></p>
-            <p className='project-description'>Meet in the Middle is a full stack app that does what it says on the tin… simply type in two locations, select a place category and radius, and hit Search. The app will calculate the midpoint of the two locations by distance and show you the top-rated places near that midpoint, which you can add to your Favourites and rate. Details of your midpoint and each nearby place will be marked on a map as well as displayed on screen.</p>
-            <p className='project-description'>I created this app not only because I always want to try new things and places but now that I live in a different area to my loved ones, it's always helpful to meet at a convenient place in the middle when we reunite. I designed and built the app with the user in mind. I recognised that although there are similar products out there, I hadn't come across a service that made it so user-friendly and visually simple to find a place and save it for later.</p>
+            <p className="project-description">Meet in the Middle is a Full Stack application where users can easily search for places in the middle of two starting points, categorised by place type, allowing them to meet others in the most convenient way. Includes several features such as:</p>
+            <p className="project-description">
+            <ul>
+              <li>Geocoding and Nearby Search features which take two user inputs and display the midpoint/Places of Interest using location APIs. Details of the top locations are visualised on screen with their Foursquare ratings, allowing users to find suitable recommendations.</li>
+              <li>A Favourites feature using a Firebase/Cloud Firestore database where users can easily ‘like’ a nearby place and save it for later use.</li>
+              <li>A Ratings features where users can come back and leave a review once they have visited a place, using a Firebase 'Ratings' collection.</li>
+              <li>Responsive information and contact pages, including a working contact form using EmailJS.</li>
+            </ul>
+            </p>
+
             <p className='project-buttons'><Button variant="contained" href="https://meet-in-the-middle.netlify.app" size="large" sx={{marginRight: "10px", fontFamily: "Montserrat", backgroundColor: "#e3997c", "&:hover": {backgroundColor: "#e8ad96", color: "#486b53"}}}>
           See the live app
           </Button>
@@ -123,36 +119,3 @@ export default function Middle() {
     </div>
   )
 }
-
-const itemData = [
-    {
-      img: Logo,
-      title: 'Meet in the Middle Logo',
-      rows: 2,
-      cols: 2,
-    },
-    {
-      img: Figma_1,
-      title: 'Figma Design 1',
-      rows: 2,
-      cols: 1
-    },
-    {
-      img: Figma_2,
-      title: 'Figma Design 2',
-      rows: 2,
-      cols: 1
-    },
-    {
-      img: LoginPage,
-      title: 'Login Page',
-      rows: 2,
-      cols: 2,
-    },
-    {
-      img: SearchPage,
-      title: 'Nearby Search Page',
-      rows: 2,
-      cols: 2,
-    },
-  ];
