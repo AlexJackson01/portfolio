fb8b5d40c65f3578e3b6d0db59cefa3c7e0a3ebe
AlexJackson01 <ajjackson22@gmail.com>
2022-03-17 16:01:07 +0000

    Tweaks for mobile

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -24,16 +24,16 @@ export default function Projects() {
             <div className='row'>
             <div className='col-lg-6 col-xs-12'>          
             <div className="project-card">
-              <img className="project-image-dev" src={MFBLogo} alt="MFB Logo" />
-              <h3>Mighty Fierce Birth</h3>
-              <p className="subtext">Front end website for Mighty Fierce Birth, a hypnobirthing business that specialises in supporting others during their pregnancies to prepare for positive birth experiences.</p>
+            <img className="project-image-dev" src={Mood} alt="MFB Logo" />
+              <h3>MoodTrack</h3>
+              <p className="subtext">Full Stack mobile application that promotes daily wellbeing check-ins and facilitates the use of music as a positive outlet for people's mental health.</p>
               <hr />
         </div></div>
         <div className='col-lg-6 col-xs-12'>
         <div className="project-card">
-              <img className="project-image-dev" src={Mood} alt="MFB Logo" />
-              <h3>MoodTrack</h3>
-              <p className="subtext">Full Stack mobile application that promotes daily wellbeing check-ins and facilitates the use of music as a positive outlet for people's mental health.</p>
+        <img className="project-image-dev" src={MFBLogo} alt="MFB Logo" />
+              <h3>Mighty Fierce Birth</h3>
+              <p className="subtext">Front end website for Mighty Fierce Birth, a hypnobirthing business that specialises in supporting others during their pregnancies to prepare for positive birth experiences.</p>
               <hr />
         </div></div>
         </div>
